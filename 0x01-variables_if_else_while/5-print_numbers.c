3e5fa07c65915235f4a2acb9250a055aa0056d9f
bonafike <fikadulikessa@gmail.com>
2022-10-17 13:24:03 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
