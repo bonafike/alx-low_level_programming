142e8fab897d9bec00665bf58ae498877f6068a4
bonafike <fikadulikessa@gmail.com>
2022-10-17 05:58:20 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,13 @@
 /**
  * main - prints and that part is useful
  *
- * Description:'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return 1(success)
+ * Return: 1 (success)
  */
+
 int main(void)
 {
-        write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
