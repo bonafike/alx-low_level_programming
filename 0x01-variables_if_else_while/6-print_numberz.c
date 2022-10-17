937913bab9e9487ec7cb9159f9c4e773af55faee
bonafike <fikadulikessa@gmail.com>
2022-10-17 13:49:25 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Numbers of base ten
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
