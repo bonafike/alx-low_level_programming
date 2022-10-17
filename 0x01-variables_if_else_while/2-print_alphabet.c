76ee9480cae2e2df2ebe19d2d53c300cc1008e41
bonafike <fikadulikessa@gmail.com>
2022-10-17 12:35:25 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ * main - alphabet game
+ *
+ * Return: always (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'b'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
