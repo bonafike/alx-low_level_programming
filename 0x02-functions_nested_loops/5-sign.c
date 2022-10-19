9616e7db3d71da5b22e7a5099ad12a8ddc82a249
bonafike <fikadulikessa@gmail.com>
2022-10-19 16:26:32 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - print sign of num
+ *
+ * @n: print int
+ *
+ * Description: print sign of num
+ *
+ *
+ * Return: 1 or 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+')
+return (1);
+}
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+else
+{
+_putchar ('-');
+return ('-1');
+}
+}
