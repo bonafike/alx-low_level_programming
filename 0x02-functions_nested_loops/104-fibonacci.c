729bd864e91d4dd5de442851a5255b063701978d
bonafike <fikadulikessa@gmail.com>
2022-10-19 19:35:45 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,18 +3,20 @@
  * main- prints first 98 fibonacci num                                
  *
  *
- * Return: Always 0 (Success)                                                   
+ * Return: Always 0 (Success)
  */
-int main(void)                                                                  
-{    
-unsigned long int i, j, k, j1, j2, k1, k2;                                      j = 1;
+int main(void) 
+{
+unsigned long int i, j, k, j1, j2, k1, k2;
+j = 1;
 k = 2;
-printf("%lu", j);                                                               for (i = 1; i < 91; i++)
+printf("%lu", j);
+for (i = 1; i < 91; i++)
 {
 printf(", %lu", k);
 k = k + j;
 j = k - j;
-}                                                                              
+}
 j1 = j / 1000000000;
 j2 = j % 1000000000;
 k1 = k / 1000000000;
