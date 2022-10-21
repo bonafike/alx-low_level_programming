558af2b46410fc6ae1dbafada4a386618093748d
bonafike <fikadulikessa@gmail.com>
2022-10-21 13:21:08 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: Fizz-Buzz
+ *
+ * Return: number
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
