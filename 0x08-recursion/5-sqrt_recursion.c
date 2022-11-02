bf2f12433d5068059db7b9d3c362d0e92f2c20e0
bonafike <fikadulikessa@gmail.com>
2022-11-02 16:41:03 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,11 +7,11 @@
  */
 int _sqrt_recur(int i, int n)
 {
-if (i * i == n)
+if (n * n == i)
 {
-return (i);
+return (n);
 }
-else if (i * i > n)
+else if (n * n > i)
 {
 return (-1);
 }
@@ -22,7 +22,7 @@ return (_sqrt_recur(i, n + 1));
  * @n: the number
  * Return: -1, 0, or sqrt
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int i)
 {
-return (_sqrt_recur(n, 0));
+return (_sqrt_recur(i, 0));
 }
