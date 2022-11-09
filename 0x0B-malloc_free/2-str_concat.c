50a192fa5e1c809a183a4415635dc629255bbe8f
bonafike <fikadulikessa@gmail.com>
2022-11-08 17:20:18 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,8 @@
 
 /**
  * str_concat - concatenates
- * @s1: 1st string
- * @s2: 2nd string
+ * @s1: first string
+ * @s2: second string
  *
  * Return: pointer of an array
  */
@@ -21,7 +21,7 @@ s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 ;
 
-for (j = 0; s2[j] !- '\0'; j++)
+for (j = 0; s2[j] != '\0'; j++)
 ;
 
 strout = malloc(sizeof(char) * (i + j + 1));
@@ -36,7 +36,7 @@ for (k = 0; k < i; k++)
 strout[k] = s1[k];
 
 limit = j;
-for (j = 0; j <= limit; k+=, j++)
+for (j = 0; j <= limit; k++, j++)
 strout[k] = s2[j];
 
 return (strout);
