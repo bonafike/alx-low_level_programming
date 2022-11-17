f8995ee03c7e35821f4bffe5566132ed2571d718
bonafike <fikadulikessa@gmail.com>
2022-11-17 06:00:06 -0800

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list ap;
 
-va_start(ap, n):
+va_start(ap, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ap, int));
-if (i != n- 1 && separator)
+if (i != n - 1 && separator)
 printf("%s", separator);
 }
 va_end(ap);
