ca6ffd3f40c5b1dc5a39ce24fa372e5a5fd6af66
bonafike <fikadulikessa@gmail.com>
2022-11-25 06:13:18 -0800

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return
  */
 
-void_attribute_((constructor)) bmain()
+void __attribute__ ((constructor)) bmain()
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back!\n");
